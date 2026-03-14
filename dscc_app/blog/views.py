ab64d28dd89873c60c71f7eee7229cfa859b8c0f
ubaydullayev <ubaydullayevvvv@gmail.com>
2026-03-14 13:38:19 +0500

    edit for testing

--- dscc_app/blog/views.py
+++ dscc_app/blog/views.py
@@ -24,7 +24,7 @@ class PostListView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["page_title"] = "Latest Posts"
+        context["page_title"] = "Latest Posts List"
         context.setdefault("is_my_posts", False)
         return context
 
